37fe961a037025151487d4f925342f19f25593a7
Tom Forbes <tom@tomforb.es>
2020-05-19 22:31:04 +0100

    WIP

--- benches/distance.rs
+++ benches/distance.rs
@@ -1,105 +1,77 @@
-
 use criterion::measurement::WallTime;
-use criterion::{
-    black_box, criterion_group, criterion_main, BenchmarkGroup, BenchmarkId, Criterion, Throughput,
-};
+use criterion::{criterion_group, criterion_main, BenchmarkGroup, BenchmarkId, Criterion, Throughput, black_box};
 use embeddingdb;
 use embeddingdb::collection::Collection;
 use nalgebra::allocator::Allocator;
-use nalgebra::{
-    distance, DefaultAllocator, DimName, Matrix2x3, MatrixMN, MatrixN, Point, Point3, Scalar,
-    VectorN, U1, U3,
-};
+use nalgebra::{DefaultAllocator, DimName, Point, VectorN, ComplexField, RealField};
 use nalgebra::{U32, U64};
 
-use num_traits::FromPrimitive;
-use rand::{thread_rng, Rng};
-
-use simba;
-use simba::scalar::{ComplexField, RealField};
+use rand::distributions::Standard;
+use rand::prelude::Distribution;
 use std::time::Duration;
 use typenum::{U128, U256, U384, U512};
 
-fn criterion_benchmark<DimX, DType>(_group: &mut BenchmarkGroup<WallTime>)
+fn criterion_benchmark<DimX, DType>(group: &mut BenchmarkGroup<WallTime>)
 where
     DimX: DimName,
     DType: ComplexField + RealField,
     // <DType as ComplexField>::RealField: ,
     DefaultAllocator: Allocator<DType, DimX>,
     <DefaultAllocator as Allocator<DType, DimX>>::Buffer: Send + Sync,
+    Standard: Distribution<DType>,
 {
-    let mut rng = thread_rng();
     let pool = rayon::ThreadPoolBuilder::new()
         .num_threads(6)
         .build()
         .unwrap();
 
-    for _size in [10_000, 100_000, 250_000, 500_000, 1_000_000, 2_000_000].iter() {
-        let collection: Collection<DType, DimX> =
-            embeddingdb::collection::Collection::new("My Collection", &pool);
+    for size in (100_000..200_000).step_by(100_000) {
+        let mut collection: Collection<DType, DimX> =
+            embeddingdb::collection::Collection::new("My Collection");
 
-        let mut random_vec: Vec<DType> = Vec::with_capacity(DimX::dim());
-        for _ in 0..(DimX::dim()) {
-            random_vec.push(DType::from_f64(rng.gen::<f64>()).unwrap());
-        }
-        collection.push(VectorN::<DType, DimX>::from_vec(random_vec).into());
-        // for _ in 0..*size {
-        //     // I HAVE NO FUCKING CLUE WHAT I AM DOING
-        //     // WHAT THE FUCK IS ANY OF THIS CODE
-        //     let mut random_vec: Vec<DType> = Vec::with_capacity(DimX::dim());
-        //     for _ in 0..(DimX::dim()) {
-        //         random_vec.push(DType::from_f64(rng.gen::<f64>()).unwrap());
-        //     }
-        //     collection.push(VectorN::<DType, DimX>::from_vec(random_vec).into());
-        // }
-        let mut random_vec: Vec<DType> = Vec::with_capacity(DimX::dim());
-        for _ in 0..(DimX::dim()) {
-            random_vec.push(DType::from_f64(rng.gen::<f64>()).unwrap());
-        }
-        let _our_point: Point<DType, DimX> = VectorN::<DType, DimX>::from_vec(random_vec).into();
-        let _comparison: <DType as ComplexField>::RealField = DType::from_f32(10.).unwrap();
-        // group.throughput(Throughput::Elements(collection.points.len() as u64));
-        // group.bench_with_input(
-        //     BenchmarkId::new(
-        //         format!("{}/{}", std::any::type_name::<DType>(), DimX::dim()),
-        //         collection.len(),
-        //     ),
-        //     &collection.points,
-        //     |b, points| {
-        //         return pool.install(|| {
-        //             b.iter(|| {
-        //                 return points
-        //                     .into_par_iter()
-        //                     .filter_map(|p| {
-        //                         // simulate comparing
-        //                         distance(&our_point, p).partial_cmp(&comparison)
-        //                     })
-        //                     // if we don't do this, nothing happens
-        //                     .count();
-        //             });
-        //         });
-        //     },
-        // );
-        // std::mem::drop(collection)
+        let random_points: Vec<Point<DType, DimX>> = (0..size)
+            .map(|_| VectorN::new_random().into())
+            .collect();
+
+        collection.extend(&random_points);
+
+        let our_point: Point<DType, DimX> = VectorN::new_random().into();
+        group.throughput(Throughput::Elements(collection.len() as u64));
+        group.bench_function(
+            BenchmarkId::new(
+                format!("{}", std::any::type_name::<DType>()),
+                format!("{}/{}", DimX::dim(), collection.len())
+            ),
+            |b| {
+                return pool.install(|| {
+                    b.iter(|| {
+                        return black_box(collection.find(&our_point, 0.5f32).len())
+                    });
+                });
+            },
+        );
+        std::mem::drop(collection)
     }
 }
 
 fn run_bench(c: &mut Criterion) {
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(6)
+        .build()
+        .unwrap();
     let mut g = c.benchmark_group("Test");
     g.warm_up_time(Duration::from_secs(10));
     g.measurement_time(Duration::from_secs(30));
-    criterion_benchmark::<U32, f32>(&mut g);
     criterion_benchmark::<U64, f32>(&mut g);
     criterion_benchmark::<U128, f32>(&mut g);
-    criterion_benchmark::<U256, f32>(&mut g);
-    criterion_benchmark::<U384, f32>(&mut g);
+    // criterion_benchmark::<U256, f32>(&mut g);
+    // criterion_benchmark::<U384, f32>(&mut g);
     criterion_benchmark::<U512, f32>(&mut g);
 
-    criterion_benchmark::<U32, f64>(&mut g);
     criterion_benchmark::<U64, f64>(&mut g);
     criterion_benchmark::<U128, f64>(&mut g);
-    criterion_benchmark::<U256, f64>(&mut g);
-    criterion_benchmark::<U384, f64>(&mut g);
+    // criterion_benchmark::<U256, f64>(&mut g);
+    // criterion_benchmark::<U384, f64>(&mut g);
     criterion_benchmark::<U512, f64>(&mut g);
     g.finish()
 }
--- src/collection/mod.rs
+++ src/collection/mod.rs
@@ -1,29 +1,28 @@
 use nalgebra::allocator::Allocator;
-use nalgebra::{DefaultAllocator, DimName, Point, Scalar};
-use rayon::ThreadPool;
+use nalgebra::{DefaultAllocator, DimName, Point, distance, ComplexField, RealField};
+use rayon::prelude::*;
 
-pub struct Collection<'a, F, DimX>
+pub struct Collection<F, DimX>
 where
-    F: Scalar,
+    F: ComplexField + RealField,
     DimX: DimName,
     DefaultAllocator: Allocator<F, DimX>,
 {
     name: String,
     points: Vec<Point<F, DimX>>,
-    pool: &'a ThreadPool,
 }
 
-impl<F, DimX> Collection<'_, F, DimX>
+impl<F, DimX> Collection< F, DimX>
 where
-    F: Scalar,
-    DimX: DimName,
+    F: ComplexField + RealField,
+    DimX: DimName + Sync,
     DefaultAllocator: Allocator<F, DimX>,
+    <DefaultAllocator as Allocator<F, DimX>>::Buffer: Send + Sync,
 {
-    pub fn new<S: Into<String>>(name: S, pool: &ThreadPool) -> Collection<F, DimX> {
+    pub fn new<S: Into<String>>(name: S) -> Collection<F, DimX> {
         Collection {
             name: name.into(),
             points: Vec::new(),
-            pool,
         }
     }
 
@@ -31,11 +30,30 @@ where
         return DimX::dim();
     }
 
-    pub fn push(mut self, point: Point<F, DimX>) {
+    pub fn push(&mut self, point: Point<F, DimX>) {
         self.points.push(point)
     }
 
-    pub fn len(self) -> usize {
+    pub fn extend(&mut self, points: &[Point<F, DimX>]) {
+        self.points.extend_from_slice(points)
+    }
+
+    pub fn len(&self) -> usize {
         self.points.len()
+    }
+
+    pub fn find(&self, point: &Point<F, DimX>, within: f32) -> Vec<&Point<F, DimX>>
+    {
+        let the_limit = F::from_f32(within).unwrap();
+        return self.points.par_iter()
+            .filter_map(|p| {
+                let distance = distance(&point, p);
+                if distance < the_limit {
+                    Some(p)
+                } else {
+                    None
+                }
+            })
+            .collect();
     }
 }
