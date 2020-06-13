780ffaa62ad75936bf36d3f12e988e1cc9dacb37
Tom Forbes <tom@tomforb.es>
2020-06-13 12:36:07 +0100

    Include IDs

---
+++ proximity/src/lib.rs
@@ -0,0 +1,28 @@
+use crossbeam_channel::Receiver;
+
+mod vec;
+mod debug;
+
+pub use vec::VecSIMDConstellation;
+
+
+pub trait Constellation: Sync + Send {
+    fn add_points(&self, points: Vec<(u128, Vec<f32>)>);
+    fn find(&self, point: Vec<f32>, within: f32) -> QueryIterator;
+
+    fn count(&self) -> usize;
+    fn dimensions(&self) -> usize;
+    fn memory_size(&self) -> usize;
+}
+
+pub struct QueryIterator {
+    receiver: Receiver<(f32, u128, Vec<f32>)>,
+}
+
+impl Iterator for QueryIterator {
+    type Item = (f32, u128, Vec<f32>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        return self.receiver.recv().ok();
+    }
+}
